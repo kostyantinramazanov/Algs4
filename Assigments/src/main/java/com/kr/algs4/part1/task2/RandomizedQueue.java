f1b4c49ea4d45d68dd2f97e596aff95b293753dd
U-dimerdzhy\cONST <cONST@dimerdzhy.(none)>
2014-02-28 01:07:56 +0200

    Added RandomizedQueue and Subset classes.

---
+++ Assigments/src/main/java/com/kr/algs4/part1/task2/RandomizedQueue.java
@@ -0,0 +1,75 @@
+package com.kr.algs4.part1.task2;
+
+import seidgewick.StdRandom;
+
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    private Deque<Item> deque;
+
+    /**
+     * construct an empty randomized queue
+     */
+    public RandomizedQueue() {
+        deque = new Deque<Item>();
+    }
+
+    /**
+     * is the queue empty?
+     */
+    public boolean isEmpty() {
+        return deque.isEmpty();
+    }
+
+    /**
+     * return the number of items on the queue
+     */
+    public int size() {
+        return deque.size();
+    }
+
+    /**
+     * add the item
+     */
+    public void enqueue(Item item) {
+        int factor = StdRandom.uniform(deque.size() + 1);
+        if (factor % 2 == 0) {
+            deque.addLast(item);
+        } else {
+            deque.addFirst(item);
+        }
+    }
+
+    /**
+     * delete and return a random item
+     */
+    public Item dequeue() {
+        int factor = StdRandom.uniform(deque.size() + 1);
+        if(factor % 2 == 0){
+            return deque.removeLast();
+        } else{
+            return deque.removeLast();
+        }
+    }
+
+    /**
+     * return (but do not delete) a random item
+     */
+    public Item sample() {
+        Item result = dequeue();
+        enqueue(result);
+        return result;
+    }
+
+    @Override
+    /**return an independent iterator over items in random order*/
+    public Iterator<Item> iterator() {
+        return deque.iterator();
+    }
+
+    /**
+     * unit testing
+     */
+    public static void main(String[] args) {
+    }
+}
---
+++ Assigments/src/main/java/com/kr/algs4/part1/task2/Subset.java
@@ -0,0 +1,31 @@
+package com.kr.algs4.part1.task2;
+
+import seidgewick.StdIn;
+import seidgewick.StdOut;
+
+public class Subset {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            StdOut.println("Not enough arguments specified. Pleae set k");
+            return;
+        }
+        int k = Integer.parseInt(args[0]);
+        if (k < 0) {
+            StdOut.println("k can not be negative");
+            return;
+        }
+        RandomizedQueue<String> queue = new RandomizedQueue<String>();
+
+        while (true) {
+            try {
+                String item = StdIn.readString();
+                queue.enqueue(item);
+            } catch (Exception e) {
+                break;
+            }
+        }
+        for (int i = 0; i < k; i++) {
+            StdOut.println(queue.dequeue());
+        }
+    }
+}
