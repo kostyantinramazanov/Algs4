8b31baa5d830f971497346baed5c01351718b755
U-dimerdzhy\cONST <cONST@dimerdzhy.(none)>
2014-03-13 01:53:21 +0200

    Close to final version. All requirements are satisfied.

--- Assigments/src/main/java/com/kr/algs4/part1/task3/Fast.java
+++ Assigments/src/main/java/com/kr/algs4/part1/task3/Fast.java
@@ -1,9 +1,6 @@
 package com.kr.algs4.part1.task3;
 
-import seidgewick.In;
-import seidgewick.Quick;
-import seidgewick.StdDraw;
-import seidgewick.StdOut;
+import seidgewick.*;
 
 import java.io.File;
 import java.util.Arrays;
@@ -27,29 +24,45 @@ public class Fast {
         StdDraw.setXscale(0, 32768);
         StdDraw.setYscale(0, 32768);
 
+        SET<Double> slopes = new SET<Double>();
+
         for (int i = 0; i < pointsCount; i++) {
             Point[] slopeOrder = points.clone();
             Arrays.sort(slopeOrder, points[i].SLOPE_ORDER);
             boolean[] isCollinear = new boolean[pointsCount];
-            for (int j = 0; j < pointsCount - 2; j++) {
+            isCollinear[0] = true;
+            int lineSize = 3;
+            double slope = .0;
+            for (int j = 1; j < pointsCount - 2; j += 1) {
                 double s0 = points[i].slopeTo(slopeOrder[j]);
                 double s1 = points[i].slopeTo(slopeOrder[j + 1]);
                 double s2 = points[i].slopeTo(slopeOrder[j + 2]);
                 if (s0 == s1 && s1 == s2) {
+                    lineSize++;
                     isCollinear[j] = true;
                     isCollinear[j + 1] = true;
                     isCollinear[j + 2] = true;
+                    slope = slopeOrder[j].slopeTo(slopeOrder[j+1]);
                 }
             }
-            for (int j = 0; j < pointsCount; j++) {
-//                if (isCollinear[j] && isCollinear[j + 1] && isCollinear[j + 2]) {
-//
-//                }
-                if(isCollinear[j]){
-                    StdOut.print(slopeOrder[j]);
+
+            if (lineSize > 3) {
+                if(!slopes.contains(slope)){
+                    slopes.add(slope);
+                    StdOut.print(String.format("%s", points[i]));
+                    points[i].draw();
+                    Point last = slopeOrder[0];
+                    for (int j = 1; j < pointsCount; j++) {
+                        if (isCollinear[j]) {
+                            last = slopeOrder[j];
+                            StdOut.print(String.format(" -> %s", last));
+                            last.draw();
+                        }
+                    }
+                    last.drawTo(points[i]);
+                    StdOut.println();
                 }
             }
-            StdOut.println();
         }
     }
 }
