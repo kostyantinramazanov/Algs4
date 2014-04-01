6505424c7a40ef335d8a87c2760ce415f2be7938
Kostyantin Ramazanov <kramazanov@provectus-it.com>
2014-04-01 16:38:26 +0200

    Final version.

--- Assigments/src/main/java/com/kr/algs4/part1/task4/Solver.java
+++ Assigments/src/main/java/com/kr/algs4/part1/task4/Solver.java
@@ -1,16 +1,12 @@
 package com.kr.algs4.part1.task4;
 
-import seidgewick.In;
-import seidgewick.LinkedQueue;
-import seidgewick.MinPQ;
-import seidgewick.StdOut;
+import seidgewick.*;
 
 import java.util.Comparator;
 
 public class Solver {
-    private final Board board;
-    private final LinkedQueue<Board> solution = new LinkedQueue<Board>();
-    private boolean isSolvable = false;
+    private Board board;
+    private LinkedStack<Board> solution;
 
     /**
      * find a solution to the initial board (using the A* algorithm)
@@ -24,49 +20,49 @@ public class Solver {
                 return o1.weight() - o2.weight();
             }
         };
-        try {
-            MinPQ<SearchNode> pq = new MinPQ<SearchNode>(solverComparator);
-            MinPQ<SearchNode> twinPq = new MinPQ<SearchNode>(solverComparator);
+        MinPQ<SearchNode> pq = new MinPQ<SearchNode>(solverComparator);
+        MinPQ<SearchNode> twinPq = new MinPQ<SearchNode>(solverComparator);
 
-            pq.insert(new SearchNode(board, null, 0));
-            twinPq.insert(new SearchNode(board.twin(), null, 0));
-            do {
-                SearchNode current = pq.delMin();
-                SearchNode twin = twinPq.delMin();
+        pq.insert(new SearchNode(board, null, 0));
+        twinPq.insert(new SearchNode(board.twin(), null, 0));
+        do {
+            SearchNode current = pq.delMin();
+            SearchNode twin = twinPq.delMin();
 
-                solution.enqueue(current.getBoard());
-                if (!current.getBoard().isGoal()) {
-                    for (Board neighbour : current.getBoard().neighbors()) {
-                        if (current.getParent() == null || current.getParent() != null && !neighbour.equals(current.getParent().getBoard())) {
-                            pq.insert(new SearchNode(neighbour, current, current.moves() + 1));
-                        }
+            //solution.enqueue(current.getBoard());
+            if (!current.getBoard().isGoal()) {
+                for (Board neighbour : current.getBoard().neighbors()) {
+                    if (current.getParent() == null || current.getParent() != null && !neighbour.equals(current.getParent().getBoard())) {
+                        pq.insert(new SearchNode(neighbour, current, current.moves() + 1));
                     }
-                } else {
-                    isSolvable = true;
-                    break;
                 }
+            } else {
+                solution = new LinkedStack<Board>();
+                SearchNode node = current;
+                while (node != null) {
+                    solution.push(node.getBoard());
+                    node = node.getParent();
+                }
+                break;
+            }
 
-                if (!twin.getBoard().isGoal()) {
-                    for (Board neighbour : twin.getBoard().neighbors()) {
-                        if (twin.getParent() == null || twin.getParent() != null && !neighbour.equals(twin.getParent().getBoard())) {
-                            twinPq.insert(new SearchNode(neighbour, twin, twin.moves() + 1));
-                        }
+            if (!twin.getBoard().isGoal()) {
+                for (Board neighbour : twin.getBoard().neighbors()) {
+                    if (twin.getParent() == null || twin.getParent() != null && !neighbour.equals(twin.getParent().getBoard())) {
+                        twinPq.insert(new SearchNode(neighbour, twin, twin.moves() + 1));
                     }
-                } else {
-                    isSolvable = false;
-                    break;
                 }
-            } while (!pq.isEmpty() || !twinPq.isEmpty());
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
+            } else {
+                break;
+            }
+        } while (!pq.isEmpty() || !twinPq.isEmpty());
     }
 
     /**
      * is the initial board solvable?
      */
     public boolean isSolvable() {
-        return isSolvable;
+        return solution() != null;
     }
 
     /**
